b8128f24464991fe784f71bd65a6b4b78093d593
pagabriel <gabriel.payet@ynov.com>
2026-06-27 04:22:42 +0200

    supabase intégration La commune

--- js/site-content-loader.js
+++ js/site-content-loader.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Remplace avec tes vraies clés de projet Supabase
 const SUPABASE_URL = 'https://eyjooultejiibshzvztm.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_K125NqFqEzjQQ5QGsrur-Q_MYjzVH1V'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5am9vdWx0ZWppaWJzaHp2enRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMjkzMDksImV4cCI6MjA5NzkwNTMwOX0.G69SkW-FpvP5RGsF6MhfXa3Jl-_OxHbfYURNo9Hqcvw'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
